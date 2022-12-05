27107107105b7159ae8526cb124acc3118cc7067
Nikita <mail@gmail.com>
2022-12-06 00:16:04 +0300

    Started Task 2

---
+++ Task_2/Program.cs
@@ -0,0 +1,70 @@
+﻿/* Напишите программу, которая на вход принимает позиции элемента в двумерном массиве,
+и возвращает значение этого элемента или же указание, что такого элемента нет.
+
+Например, задан массив:
+
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет */
+
+Console.Clear();
+int rows = UserInput("Введите количество строк в массиве: ", "Введено неверное значение!");
+int columns = UserInput("Введите количество столбцов в массиве: ", "Введено неверное значение!");
+int[,] array = ArrayOfRealNumbers(rows, columns, -100, 100);
+PrintArray(array);
+
+int position = UserInput("Введите позицию необходимого числа в массиве: ", "Введено неверное значение!");
+Console.Write($"{position} -> ");
+PositionOfNumber(array);
+
+
+void PositionOfNumber(int[,] inArray)
+{
+    for (int i = 0; i < inArray.Length; i++)
+    {
+        if (i == (position - 1))
+        {
+            Console.WriteLine($"{i}");
+            break;
+        }
+    }
+    Console.WriteLine("такого числа в массиве нет!");
+}
+
+int[,] ArrayOfRealNumbers(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] arr = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            arr[i,j] = new Random().Next(minValue, maxValue + 1);
+        }
+    }
+    return arr;
+}
+
+void PrintArray(int[,] inArray)
+{
+    for (int i = 0; i < inArray.GetLength(0); i++)
+    {
+        for (int j = 0; j < inArray.GetLength(1); j++)
+        {
+            Console.Write($"{inArray[i,j]}  ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int UserInput(string message, string errorMessage)
+{
+    while (true)
+    {
+        Console.Write(message);
+        if (int.TryParse(Console.ReadLine()??"", out int number)) return number;
+        else Console.WriteLine(errorMessage);
+    }
+}
